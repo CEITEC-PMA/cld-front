c1d4c0899ffb419df8d2cfa695eafaa2d66dc517
Raphael Melo <raphaelcmelob.b@gmail.com>
2024-01-15 11:36:51 -0300

    corrige textos

--- src/app/dashboard/registro/page.tsx
+++ src/app/dashboard/registro/page.tsx
@@ -100,13 +100,12 @@ const UnidadeRegistro: React.FC<Props> = ({ onSubmit }) => {
 
   const handleBuscaCep = async (cep: string) => {
     if (!isValidCEP) {
-      console.log("Cep inválido");
+      alert("CEP inválido");
       return;
     }
     try {
       const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
       const resJson = await response.json();
-      console.log(resJson);
 
       setValue("endereco.logradouro", resJson.logradouro);
       setValue("endereco.bairro", resJson.bairro);
