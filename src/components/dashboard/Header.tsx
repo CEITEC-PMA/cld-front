8cf35aea5cc1e5974f13016c8539df42393fe637
Raphael Melo <raphaelcmelob.b@gmail.com>
2024-02-02 11:09:33 -0300

    style: change drawer to open by default

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 import { apiUrl } from "@/utils/api";
 
 export default function Header() {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
   const toggleDrawer = () => {
     setOpen(!open);
   };
